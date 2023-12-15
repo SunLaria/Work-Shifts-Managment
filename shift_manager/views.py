d9bcb8e13d171433c90a9224feddf2fef2483f0e
RandomG <sun4151@gmail.com>
2023-12-15 20:51:14 +0200

    delete variable view

--- shift_manager/views.py
+++ shift_manager/views.py
@@ -12,7 +12,7 @@ def home(request,week_num=datetime.datetime.now().isocalendar()[1]-1):
     for i in range(1, 7): 
         day = startdate + datetime.timedelta(days=i)
         dates.append(day.strftime('%d-%m-%Y')) 
-    return render(request, "shift_manager/index.html", {"week_dates":dates,"week_num":week_num,"workers_list":[{"Worker_ID":i.Worker_ID,"Full_Name":i.Full_Name} for i in Worker.objects.all()],"shifts_data":Shift.objects.all()})
+    return render(request, "shift_manager/index.html", {"week_dates":dates,"week_num":week_num,"workers_list":[{"Worker_ID":i.Worker_ID,"Full_Name":i.Full_Name} for i in Worker.objects.all()]})
 
 def save(request):
     if request.method=="POST":
