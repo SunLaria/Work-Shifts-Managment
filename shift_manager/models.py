ca45ed6ea024b6cf9f754cf3623f16952d428720
Sun <40611674+SunLaria@users.noreply.github.com>
2023-12-23 20:57:31 +0200

    Update models.py

--- shift_manager/models.py
+++ shift_manager/models.py
@@ -9,32 +9,9 @@ class Worker(models.Model):
     Full_Name = models.CharField(max_length=20)
     Work_Days = models.IntegerField()
     
-    # def shift_count(self,start_date,end_date):
-    #     if len(self.objects.Shift_set.filter().filter(recordDate__gte=start_date, recordDate__lte=end_date)) <= 5:
-    #         return True
-    #     else:
-    #         return False
 
 
 class Shift(models.Model):
     Date = models.DateField("shift date")
     Shift = models.CharField(max_length=10, choices=[("mo","Morning"),("af","Afternoon"),("ni","Night")])
     Worker = models.ForeignKey(Worker,on_delete=models.CASCADE)
-
-    #  choices=[(str(i.Worker_ID), str(i.Full_Name)) for i in Worker.objects.all()]
-  
-# class WorkerForm(forms.Form):
-#     class Meta:
-#         model = Worker
-#         fields = ["Worker_ID", "Full_Name", "Work_Days"]
-
-# class Shift(forms.Form):
-#     class Meta:
-#         model = Shift
-#         fields = ["Date", "Shift", "Worker"]
-
-
-# q.get_Shift_display() - מראה את הבחירה בצורה המלאה לפי ה choices
-# q.clean_fields() - בודק את כל הפרמרטרים עם validators
-
-# Worker.objects.create(Worker_ID=23223,Full_Name="Sun Laria",Work_Days=6)
